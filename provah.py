f89b2110b7f2e6b7e103c97ee1c452f0e3a5c0da
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-06-12 21:54:37 +0200

    Update provah.py

--- provah.py
+++ provah.py
@@ -23,17 +23,7 @@ language = st.radio( "Виберіть мову - Выберите язык" , (
 
 if language == 'Русский':
   placechoice = st.selectbox("Куда ты хочешь пойти сегодня? Bыбери одно из указанных мест:", ('Площадка для игр', 'Школа', 'Магазин канцтоваров'))
-  if placechoice == 'Площадка для игр':
-      image1 = Image.open('playground.jpg')
-      st.image(image1, caption='Photo by Pond Juprasong on Unsplash')
-  if placechoice == 'Школа':
-      image2 = Image.open('school.jpg')
-      st.image(image2, caption='Photo by Kenny Eliason on Unsplash')
-  if placechoice == 'Магазин канцтоваров':
-      image3 = Image.open('stationary_shop.jpg')
-      st.image(image3, caption='Photo by @candelarms on Unsplash')
-  else:
-    pass
+
 cola, colb, colc, cold = st.columns(4)
 with cola:
   st.subheader("Полезные выражения")
